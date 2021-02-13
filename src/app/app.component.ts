2ccec7f8c71c38222a8c39b5327cb8bfca530835
Alejandro Benjumea Aguirre <alejo120792120792@hotmail.com>
2021-02-13 18:44:35 -0500

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  titulo: string = 'Contador App';
+
+  numero: number = 0;
+
+  base: number = 5;
+
+  // tslint:disable-next-line:typedef
+  acumular( valor: number ) {
+    this.numero += valor;
+  }
+
 }
